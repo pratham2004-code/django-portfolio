c16ae3c03afa0e217529bc14701fbc056279692e
Pratham Sharma <pratham2149@gmail.com>
2026-03-29 21:28:19 +0530

    fix proxy settings

--- core/settings.py
+++ core/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://django-portfolio-production-b515.up.railway.app']
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definition
 
